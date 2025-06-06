02b15f6a610284da1d8c12866ac0dbb868102eb8
Laury Pintos <pintoslaury13@gmail.com>
2025-06-06 19:31:52 -0300

    added language and skills section

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ window.addEventListener("scroll", () => {
 window.onload = function () {
   window.scrollTo(0, 0);
 };
+
+//change lenguage
+const langButtons = document.querySelectorAll("[data-language]");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+langButtons.forEach((button) => {
+  button.addEventListener("click", ()=>{
+     // Quitar clase activa de todos los botones
+    langButtons.forEach((btn) => btn.classList.remove("active-lang"));
+    // Agregar clase activa al botón clickeado
+    button.classList.add("active-lang");
+
+    
+    fetch(`../lenguages/${button.dataset.language}.json`)
+      .then(res => res.json())
+      .then(data => {
+        textsToChange.forEach((el)=>{
+          const section = el.dataset.section
+          const value = el.dataset.value
+          
+          el.innerHTML = data[section][value];
+
+        })
+      })
+  })
+})
